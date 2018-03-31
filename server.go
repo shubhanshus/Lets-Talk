6710ec0867aee66ffd01b79028206655cc6970cc
Yingxi Cao <caocasey@gmail.com>
2018-03-31 15:54:59 -0400

    -Add login and signup pages -Add jquery to check user inputs including checking if 2 input passwords are the same.

--- server.go
+++ server.go
@@ -15,7 +15,8 @@ type PageVariables struct {
 func main() {
   //html path
 	http.HandleFunc("/", Index)
-
+  http.HandleFunc("/login", Login)
+  http.HandleFunc("/signup", Signup)
   //resource path
   http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
   http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
@@ -40,4 +41,38 @@ func Index(w http.ResponseWriter, r *http.Request){
     if err != nil { // if there is an error
   	  log.Print("template executing error: ", err) //log it on terminal
   	}
+}
+
+func Login(w http.ResponseWriter, r *http.Request){
+
+    LoginPageVars := PageVariables{ //store the date and time in a struct
+      
+    }
+  
+    t, err := template.ParseFiles("templates/login.html") //parse the html file 
+    if err != nil { // if there is an error
+      log.Print("template parsing error: ", err) // log it on terminal
+    }
+    err = t.Execute(w, LoginPageVars) //execute the template and pass it to index page
+    if err != nil { // if there is an error
+      log.Print("template executing error: ", err) //log it on terminal
+    }
+    
+}
+
+func Signup(w http.ResponseWriter, r *http.Request){
+
+    SignupPageVars := PageVariables{ //store the date and time in a struct
+      
+    }
+  
+    t, err := template.ParseFiles("templates/signup.html") //parse the html file 
+    if err != nil { // if there is an error
+      log.Print("template parsing error: ", err) // log it on terminal
+    }
+    err = t.Execute(w, SignupPageVars) //execute the template and pass it to index page
+    if err != nil { // if there is an error
+      log.Print("template executing error: ", err) //log it on terminal
+    }
+    
 }
