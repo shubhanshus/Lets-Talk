409b7d59bf7a7614bfcb1f1667e1c74ab803e728
Yingxi Cao <caocasey@gmail.com>
2018-03-31 23:11:15 -0400

    -Add like button -Add cancel account page

--- server.go
+++ server.go
@@ -17,6 +17,8 @@ func main() {
 	http.HandleFunc("/", Index)
   http.HandleFunc("/login", Login)
   http.HandleFunc("/signup", Signup)
+  http.HandleFunc("/cancel", Cancel)
+
   //resource path
   http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
   http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
@@ -71,6 +73,24 @@ func Signup(w http.ResponseWriter, r *http.Request){
       log.Print("template parsing error: ", err) // log it on terminal
     }
     err = t.Execute(w, SignupPageVars) //execute the template and pass it to index page
+    if err != nil { // if there is an error
+      log.Print("template executing error: ", err) //log it on terminal
+    }
+    
+}
+
+
+func Cancel(w http.ResponseWriter, r *http.Request){
+
+    CancelPageVars := PageVariables{ //store the date and time in a struct
+      
+    }
+  
+    t, err := template.ParseFiles("templates/cancel.html") //parse the html file 
+    if err != nil { // if there is an error
+      log.Print("template parsing error: ", err) // log it on terminal
+    }
+    err = t.Execute(w, CancelPageVars) //execute the template and pass it to index page
     if err != nil { // if there is an error
       log.Print("template executing error: ", err) //log it on terminal
     }
