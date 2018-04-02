343ecedd946645f0bdd8b84fbe4733e48a7694b3
Shubhanshu Surana <shubhanshu.surana@gmail.com>
2018-04-02 11:02:26 -0400

    removed appengine dependency and did some refactoring

--- tweet.go
+++ tweet.go
@@ -2,8 +2,6 @@ package main
 
 
 import (
-"google.golang.org/appengine"
-"google.golang.org/appengine/datastore"
 "net/http"
 )
 
@@ -12,18 +10,18 @@ func putTweet(req *http.Request, user *user, tweet *tweet) {
 }
 
 func getTweets(req *http.Request, user *user) ([]tweet, error) {
-	ctx := appengine.NewContext(req)
-
+	//ctx := appengine.NewContext(req)
+	//
 	var tweets []tweet
-	q := datastore.NewQuery("Tweets")
-
-	if user != nil {
-		// show tweets of a specific user
-		userKey := datastore.NewKey(ctx, "Users", user.UserName, 0, nil)
-		q = q.Ancestor(userKey)
-	}
-
-	q = q.Order("-Time").Limit(20)
-	_, err := q.GetAll(ctx, &tweets)
-	return tweets, err
+	//q := datastore.NewQuery("Tweets")
+	//
+	//if user != nil {
+	//	// show tweets of a specific user
+	//	userKey := datastore.NewKey(ctx, "Users", user.UserName, 0, nil)
+	//	q = q.Ancestor(userKey)
+	//}
+	//
+	//q = q.Order("-Time").Limit(20)
+	//_, err := q.GetAll(ctx, &tweets)
+	return tweets, nil
 }
