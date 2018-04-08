a0049314724902b0c62db06f2be42afcb18bca5e
Shubhanshu Surana <shubhanshu.surana@gmail.com>
2018-04-08 12:23:33 -0400

    added new test case for signup and delete user functionality

--- main_test.go
+++ main_test.go
@@ -7,12 +7,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestHealthCheckHandler(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, client")
+		fmt.Fprintln(w, "Hello server is running")
 	}))
 	defer ts.Close()
 
@@ -35,7 +36,7 @@ func TestHealthCheckHandler(t *testing.T) {
 func TestSignupChcek(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, client")
+		fmt.Fprintln(w, "Hello server is running")
 	}))
 	defer ts.Close()
 
@@ -53,4 +54,33 @@ func TestSignupChcek(t *testing.T) {
 	}
 
 	fmt.Printf("%s", greeting)
+}
+
+func  TestUserSignupCheck(t *testing.T){
+	p1:="help"
+	bs, err := bcrypt.GenerateFromPassword([]byte(p1), bcrypt.MinCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var us1 =user{};
+	var user= user{
+		UserName:"jj@gmail.com",
+		First:"JJ",
+		Last:"Help",
+		Password:bs,
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		createSession(w,r,user)
+		us1=getUser(w,r)
+		if len(us1.UserName)<1{
+			t.Errorf("User addition failed")
+		}
+		dbUsers[user.UserName] = u
+		cancel(w,r)
+		if len(dbSessions)>0 ||len(dbUsers)>0{
+			t.Errorf("User deletion failed")
+		}
+	}))
+	defer ts.Close()
+
 }
