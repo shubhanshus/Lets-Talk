86c2fc46904de2a7b87212cb36732e166ff6acfe
Shubhanshu Surana <shubhanshu.surana@gmail.com>
2018-04-22 20:44:34 -0400

    added test case for follow functionality

--- server/server_test.go
+++ server/server_test.go
@@ -86,6 +86,54 @@ func TestPostTalk(t *testing.T){
 
 }
 
+func TestFollowUser(t *testing.T){
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("did not connect: %v", err)
+	}
+	c := pb.NewLetstalkClient(conn)
+	defer conn.Close()
+
+	var u pb.User
+	u.Email="test2@test.com"
+	u.Password1="test"
+	u.Lastname="test"
+	u.Firstname="test"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	resp, err := c.SendSignup(ctx, &pb.SignupRequest{User:&u})
+	if err != nil {
+		t.Errorf("Signup 2 Failed")
+	}
+	log.Println(resp.Message)
+	var talk1 pb.Talk
+	talk1.Email="test@test.com"
+	talk1.Talk="Test message"
+	talk1.Date=time.Now().Format("02-01-2006")+" "+time.Now().Format("15:04PM")
+	// Contact the server and print out its response.
+	defer cancel()
+	res, err := c.SendTalk(ctx, &pb.TalkRequest{Talk: &talk1})
+	if err != nil {
+		t.Errorf("Post tweet Failed")
+	}
+	log.Println(res.Message)
+
+	var ud []string
+	ud= append(ud, u.Email)
+	ud =append(ud,"test@test.com")
+	r, err := c.FollowUsers(ctx, &pb.FollowUserRequest{Username:u.Email,Email: ud})
+	if err != nil {
+		t.Errorf("Follow Failed")
+	}
+
+	log.Println(r.Talk)
+	log.Println(len(r.Talk))
+	if len(r.Talk)!=2{
+		t.Errorf("Follow functionality not working")
+	}
+}
+
 func TestAccountCancel(t *testing.T){
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
